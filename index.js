7e96636da37f707b19309f751a43af38ad54cbaa
mowaheedsatu99-sys <mowaheedsatu99@gmail.com>
2025-12-24 14:30:06 +0700

    edit baris 9

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-let = 'perubahan1'
+let cek1= 'perubahan1'
 
 
 
 console.log('perubahan dari branch coba')
 console.log('perubahan dari kolaborator mowaheed')
 
-let = 'perubahan dari branch coba-lagi'
+let cek = 'perubahan dari branch coba-lagi'
+console.log(cek)
